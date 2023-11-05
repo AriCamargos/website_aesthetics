0dc1d351efdc6f86b949aee0d861e77a4fbd3482
Ariana Camargos <arianacamargos16@gmail.com>
2023-11-05 18:13:43 -0300

    style: format

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-const kTextColor = Color(0xff707070);
+const kTextColor =  Color(0xff707070);
 const kTextLightColor = Color(0xFF555555);
 
 const kDefaultPadding = 20.0;
