86f2b209cdfbe20f93480f8017a01bb420b02e6c
Ariana Camargos <arianacamargos16@gmail.com>
2023-11-15 23:21:58 -0300

    feat: add feedback section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:web/constants.dart';
 import 'package:web/sections/about/about_section.dart';
+import 'package:web/sections/contact/contact_section.dart';
+import 'package:web/sections/feedback/feedback_section.dart';
+import 'package:web/sections/recent_work/recent_work_section.dart';
 import 'package:web/sections/services/service_section.dart';
 import 'package:web/sections/top_section/top_section.dart';
 
@@ -13,13 +16,12 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(children: const [
           TopSection(),
-          SizedBox(height: kDefaultPadding * 2,),
+          SizedBox(height: kDefaultPadding * 2),
           AboutSection(),
           ServiceSection(),
-          SizedBox(
-            height: 500,
-          ),
-          
+          RecentWorkSection(),
+          FeedbackSection(),
+          ContactSection(),
         ]),
       ),
     );
