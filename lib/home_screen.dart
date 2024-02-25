426f9657d08e794bad171c26f55e819c144ca838
Ariana Camargos <arianacamargos16@gmail.com>
2024-02-25 20:50:18 -0300

    feat: correções home e main

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:web/constants.dart';
 import 'package:web/sections/about/about_section.dart';
-import 'package:web/sections/contact/contact_section.dart';
+import 'package:web/sections/local/local_section.dart';
+import 'package:web/sections/questions/components/questions_section.dart';
+import 'package:web/sections/treatments/treatments_section.dart';
 import 'package:web/sections/feedback/feedback_section.dart';
 import 'package:web/sections/footer/footer_section.dart';
 import 'package:web/sections/recent_work/recent_work_section.dart';
 import 'package:web/sections/services/service_section.dart';
 import 'package:web/sections/top_section/top_section.dart';
+
+import 'components/banner.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,16 +19,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 20.0,
+        backgroundColor: Colors.green,
+        autofocus: true,
+        isExtended: true,
+        child: const Center(
+          child: FaIcon(FontAwesomeIcons.whatsapp),
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Column(children: const [
-          TopSection(),
-          SizedBox(height: kDefaultPadding * 2),
-          AboutTreatments(),
-          RecentWorkSection(),
-          ServiceSection(),
-          FeedbackSection(),
-          ContactSection(),
-          FooterSection(),
+        child: Column(children: [
+          const TopSection(),
+          BannerList(pathToImages: bannerList),
+          const SizedBox(height: kDefaultPadding * 2),
+          const TreatmentsSection(),
+          const AboutSection(),
+          const RecentWorkSection(),
+          const ServiceSection(),
+          const FeedbackSection(),
+          const QuestionsSection(),
+          const LocalSection(),
+          const FooterSection(),
         ]),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:web/constants.dart';
 import 'package:web/home_screen.dart';
-import 'package:web/sections/about/about_section.dart';
+import 'package:web/sections/feedback/feedback_section.dart';
+import 'package:web/sections/footer/footer_section.dart';
+import 'package:web/sections/local/local_section.dart';
+import 'package:web/sections/questions/components/questions_section.dart';
+import 'package:web/sections/recent_work/recent_work_section.dart';
+import 'package:web/sections/top_section/top_section.dart';
+import 'package:web/sections/treatments/treatments_section.dart';
 import 'package:web/sections/services/service_section.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:web/sections/about/about_section.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +19,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: const [
+          Breakpoint(start: 0, end: 360, name: 'small_mobile'),
+          Breakpoint(start: 361, end: 450, name: MOBILE),
+          Breakpoint(start: 451, end: 800, name: TABLET),
+          Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         inputDecorationTheme: kDefaultInputDecorationTheme,
@@ -25,8 +41,15 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomeScreen(),
       routes: {
-        '/about': (context) => const AboutTreatments(),
+        '/top': (context) => const TopSection(),
+        '/treatments': (context) => const TreatmentsSection(),
+        '/about': (context) => const AboutSection(),
+        '/work': (context) => const RecentWorkSection(),
         '/sevice': (context) => const ServiceSection(),
+        '/feedback': (context) => const FeedbackSection(),
+        '/questions': (context) => const QuestionsSection(),
+        '/local': (context) => const LocalSection(),
+        '/footer': (context) => const FooterSection(),
       },
     );
   }
