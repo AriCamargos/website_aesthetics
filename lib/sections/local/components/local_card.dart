0dc1ecbd9f3afd5240190d62144b5cf744d0cfdc
Ariana Camargos <arianacamargos16@gmail.com>
2024-02-25 20:38:28 -0300

    feat: add nova section de localização

---
+++ lib/sections/local/components/local_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class LocalCard extends StatelessWidget {
+  const LocalCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/sections/local/local_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:web/sections/local/components/local_card.dart';
+import '../../../constants.dart';
+
+import '../components/main_title.dart';
+
+class LocalSection extends StatelessWidget {
+  const LocalSection({super.key, Key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding * 2.5),
+      constraints: const BoxConstraints(maxWidth: 1110),
+      child: Column(
+        children: [
+          MainTitle(
+            title: 'Onde estamos'.toUpperCase(),
+            subtitle: 'Av Benedito Alves Turíbio 1838 - Osasco - São Paulo',
+          ),
+          const SizedBox(height: kDefaultPadding),
+          Row(
+            children: const [
+              LocalCard(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
