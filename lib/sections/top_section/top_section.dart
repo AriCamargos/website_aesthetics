ec0424e28976896e74274f9490b381dab8d138ce
Ariana Camargos <arianacamargos16@gmail.com>
2024-03-02 23:51:05 -0300

    feat: navegação de section

--- lib/sections/top_section/top_section.dart
+++ lib/sections/top_section/top_section.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:web/constants.dart';
 
-import '../components/menu.dart';
-
 class TopSection extends StatelessWidget {
-  const TopSection({super.key});
+  final ValueChanged<int> onMenuClick;
+
+  const TopSection({super.key, required this.onMenuClick});
 
   @override
   Widget build(BuildContext context) {
     //Size size = MediaQuery.of(context).size;
     return Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [kDefaultShadow],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                'assets/images/logo1.png',
-                color: Colors.black,
-              ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [kDefaultShadow],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'assets/images/logo1.png',
+              color: Colors.black,
             ),
-            const Menu(),
-          ],
-        )
-
-        // height: 500,
-        // alignment: Alignment.center,
-        // constraints: const BoxConstraints(maxHeight: 700, minHeight: 700),
-        // width: double.infinity,
-        /*decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('/images/write-page.png'),
-          fit: BoxFit.cover,
-        ),
-      ),*/
-        );
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding:
+                const EdgeInsets.symmetric(horizontal: kDefaultPadding * 2.5),
+            constraints: const BoxConstraints(maxWidth: 1110),
+            height: 50,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              children: [
+                TextButton(
+                  child: const Text('Tratamentos'),
+                  onPressed: () => onMenuClick(1),
+                ),
+                TextButton(
+                  child: const Text('Dra Raissa'),
+                  onPressed: () => onMenuClick(2),
+                ),
+                TextButton(
+                  child: const Text('Localização'),
+                  onPressed: () => onMenuClick(3),
+                ),
+                TextButton(
+                  child: const Text('Contato'),
+                  onPressed: () => onMenuClick(4),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
