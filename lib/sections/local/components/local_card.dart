0a714615941916b4c89d35c2b8331c4f8c69f661
Ariana Camargos <arianacamargos16@gmail.com>
2024-03-02 23:50:20 -0300

    feat: deleta nomes de arquivos

--- lib/sections/local/components/local_card.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class LocalCard extends StatelessWidget {
-  const LocalCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
