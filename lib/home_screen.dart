d9d1c104269dbdf2b0682bd29f46357e7fdb3201
Ariana Camargos <arianacamargos16@gmail.com>
2024-03-02 23:48:52 -0300

    feat: navegação entre sections

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:web/constants.dart';
 import 'package:web/sections/about/about_section.dart';
+import 'package:web/sections/components/menu.dart';
 import 'package:web/sections/local/map_section.dart';
 import 'package:web/sections/questions/components/questions_section.dart';
 import 'package:web/sections/treatments/treatments_section.dart';
@@ -12,12 +13,30 @@ import 'package:web/sections/services/service_section.dart';
 import 'package:web/sections/top_section/top_section.dart';
 import 'package:web/repository/whatsapp_repository.dart';
 
-
-
 import 'components/banner.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final scrollController = ScrollController();
+
+  final keySection1 = GlobalKey();
+  final keySection2 = GlobalKey();
+  final keySection3 = GlobalKey();
+  final keySection4 = GlobalKey();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,24 +52,47 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          const TopSection(),
-          BannerList(pathToImages: bannerList),
-          const SizedBox(height: kDefaultPadding * 2),
-          const TreatmentsSection(),
-          const AboutSection(),
-          const RecentWorkSection(),
-          const ServiceSection(),
-          const FeedbackSection(),
-          const QuestionsSection(),
-           const MapSection( ),
-          const FooterSection(),
-        ]),
+        controller: scrollController,
+        child: Column(
+          children: [
+            TopSection(onMenuClick: _onMenuClick),
+            BannerList(pathToImages: bannerList),
+            const SizedBox(height: kDefaultPadding * 2),
+            TreatmentsSection(key: keySection1),
+            const RecentWorkSection(),
+            const ServiceSection(),
+            AboutSection(key: keySection2),
+            const FeedbackSection(),
+            const QuestionsSection(),
+            MapSection(key: keySection3),
+            const FooterSection(),
+          ],
+        ),
       ),
     );
   }
+
+  void _onMenuClick(int value) {
+    final RenderBox renderBox;
+
+    switch (value) {
+      case 1:
+        renderBox = keySection1.currentContext!.findRenderObject() as RenderBox;
+        break;
+      case 2:
+        renderBox = keySection2.currentContext!.findRenderObject() as RenderBox;
+        break;
+      case 3:
+        renderBox = keySection3.currentContext!.findRenderObject() as RenderBox;
+        break;
+        case 4:
+        renderBox = keySection4.currentContext!.findRenderObject() as RenderBox;
+        break;
+      default:
+        throw Exception();
+    }
+    final offset = renderBox.localToGlobal(Offset.zero);
+    scrollController.animateTo(offset.dy,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+  }
 }
-
-
-
-
--- lib/sections/components/menu.dart
+++ lib/sections/components/menu.dart
@@ -1,78 +1,39 @@
 import 'package:flutter/material.dart';
-
 import '../../constants.dart';
 
-class Menu extends StatefulWidget {
-  const Menu({super.key});
+class Menu extends StatelessWidget {
+  final ValueChanged<int> onMenuClick;
 
-  @override
-  State<Menu> createState() => _MenuState();
-}
+  const Menu({super.key, required this.onMenuClick});
 
-class _MenuState extends State<Menu> {
-  int selectedIndex = 0;
-  int hoverIndex = 0;
-  List<String> menuItens = [
-    'Home',
-    'Sobre',
-    'Serviços',
-    'Localização',
-    'Contato',
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      //espaço dentro do menu
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding * 2.5),
-      //epaço fora do menu
       constraints: const BoxConstraints(maxWidth: 1110),
       height: 50,
-
       width: MediaQuery.of(context).size.width,
-      
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: List.generate(
-          menuItens.length,
-          (index) => buildMenuItem(index),
-        ),
+        children: [
+          TextButton(
+            child: const Text('Tratamentos'),
+            onPressed: () => onMenuClick(1),
+          ),
+          TextButton(
+            child: const Text('Benefícios'),
+            onPressed: () => onMenuClick(2),
+          ),
+          TextButton(
+            child: const Text('Dra Raissa'),
+            onPressed: () => onMenuClick(3),
+          ),
+          TextButton(
+            child: const Text('Contato'),
+            onPressed: () => onMenuClick(4),
+          ),
+        ],
       ),
     );
   }
-
-  Widget buildMenuItem(int index) => InkWell(
-        onTap: () {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-        onHover: (value) {
-          setState(() {
-            value ? hoverIndex = index : hoverIndex = selectedIndex;
-          });
-        },
-        child: Container(
-          constraints: const BoxConstraints(minWidth: 122),
-          height: 100,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Text(
-                menuItens[index],
-                style: const TextStyle(fontSize: 15, color: kTextColor),
-              ),
-              //Hover Home
-              AnimatedPositioned(
-                left: 0,
-                right: 0,
-                bottom:
-                    selectedIndex != index && hoverIndex == index ? -2 : -32,
-                duration: const Duration(milliseconds: 200),
-                child: Image.asset('/images/hover.png'),
-              ),
-            ],
-          ),
-        ),
-      );
 }
