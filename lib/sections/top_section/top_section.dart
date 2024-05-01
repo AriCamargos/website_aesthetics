e38f6cc29dbe9715ef8bb80004f70f64afbcc196
AriCamargos <arianacamargos16@gmail.com>
2024-04-30 22:52:32 -0300

    feat: appbar

--- lib/sections/top_section/top_section.dart
+++ lib/sections/top_section/top_section.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:web/sections/footer/menu_button.dart';
+import 'package:web/constants.dart';
+import 'package:web/home_screen_state.dart';
 
-class TopSection extends StatelessWidget {
+class TopSection extends StatefulWidget {
   final ValueChanged<int> onMenuClick;
 
-  const TopSection({super.key, required this.onMenuClick});
+  TopSection({super.key, required this.onMenuClick});
+
+  @override
+  State<TopSection> createState() => _TopSectionState();
+}
+
+class _TopSectionState extends State<TopSection> {
+  final List _isHovering = <bool>[
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final state = MenuClickState();
+    var screenSize = MediaQuery.of(context).size;
+
     return Container(
       height: 100,
       width: double.infinity,
@@ -35,11 +55,195 @@ class TopSection extends StatelessWidget {
               height: 60,
             ),
           ),
-          const Spacer(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              MenuButton(
+          SizedBox(width: screenSize.width / 15),
+          InkWell(
+            onTap: () {},
+            onHover: (value) {
+              setState(() {
+                setState(() {
+                  value ? _isHovering[0] = true : _isHovering[0] = false;
+                });
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Home',
+                  style: TextStyle(
+                    color: _isHovering[0] ? kColorGold : Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Visibility(
+                  maintainAnimation: true,
+                  maintainState: true,
+                  maintainSize: true,
+                  visible: _isHovering[0],
+                  child: Container(
+                    height: 2,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: screenSize.width / 15),
+          InkWell(
+            onTap: () {},
+            onHover: (value) {
+              setState(() {
+                setState(() {
+                  value ? _isHovering[1] = true : _isHovering[1] = false;
+                });
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Tratamentos',
+                  style: TextStyle(
+                    color: _isHovering[1] ? kColorGold : Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Visibility(
+                  maintainAnimation: true,
+                  maintainState: true,
+                  maintainSize: true,
+                  visible: _isHovering[1],
+                  child: Container(
+                    height: 2,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: screenSize.width / 15),
+          InkWell(
+            onTap: () {},
+            onHover: (value) {
+              setState(() {
+                setState(() {
+                  value ? _isHovering[2] = true : _isHovering[2] = false;
+                });
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Procedimentos',
+                  style: TextStyle(
+                    color: _isHovering[2] ? kColorGold : Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Visibility(
+                  maintainAnimation: true,
+                  maintainState: true,
+                  maintainSize: true,
+                  visible: _isHovering[2],
+                  child: Container(
+                    height: 2,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: screenSize.width / 15),
+          InkWell(
+            onTap: () {},
+            onHover: (value) {
+              setState(() {
+                setState(() {
+                  value ? _isHovering[3] = true : _isHovering[3] = false;
+                });
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Dra Raissa',
+                  style: TextStyle(
+                    color: _isHovering[3] ? kColorGold : Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Visibility(
+                  maintainAnimation: true,
+                  maintainState: true,
+                  maintainSize: true,
+                  visible: _isHovering[3],
+                  child: Container(
+                    height: 2,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: screenSize.width / 15),
+          InkWell(
+            onTap: () {},
+            onHover: (value) {
+              setState(() {
+                setState(() {
+                  value ? _isHovering[4] = true : _isHovering[4] = false;
+                });
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Localização',
+                  style: TextStyle(
+                    color: _isHovering[4] ? kColorGold : Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Visibility(
+                  maintainAnimation: true,
+                  maintainState: true,
+                  maintainSize: true,
+                  visible: _isHovering[4],
+                  child: Container(
+                    height: 2,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+             /* MenuButton(
                 text: 'Home',
                 number: 1,
                 colors: Colors.black,
@@ -73,11 +277,4 @@ class TopSection extends StatelessWidget {
                 colors: Colors.black,
                 onPressed: () => onMenuClick(5),
                 fontSize: 20,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
+              ),*/
