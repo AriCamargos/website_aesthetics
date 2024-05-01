b4ae66dcc9d53c298cc930a0f2b652a2aa745d60
AriCamargos <arianacamargos16@gmail.com>
2024-04-30 22:52:56 -0300

    feat: appBar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,7 @@ import 'package:web/sections/questions/components/questions_section.dart';
 import 'package:web/sections/appointments/appointments_section.dart';
 import 'package:web/sections/feedback/feedback_section.dart';
 import 'package:web/sections/footer/footer_section.dart';
+import 'package:web/sections/top_section/menu_drawer.dart';
 import 'package:web/sections/treatments/treatments_section.dart';
 import 'package:web/sections/top_section/top_section.dart';
 import 'package:web/repository/whatsapp_repository.dart';
@@ -30,8 +31,25 @@ class _HomeScreenState extends State<HomeScreen> {
     final state = MenuClickState();
 
     final isMobile = MediaQuery.of(context).size.width < kMdBreakpoint;
-
+    
     return Scaffold(
+      appBar: !isMobile
+          ? PreferredSize(
+              preferredSize: const Size(double.infinity, 600),
+              child: TopSection(onMenuClick: state.onMenuClick),
+            )
+          : AppBar(
+              iconTheme: const IconThemeData(color: kColorGold),
+              backgroundColor: Colors.white,
+              elevation: 0,
+              centerTitle: true,
+              title: const Text('Dra Raissa Campos'),
+            ),
+      drawer: isMobile
+          ? MenuDrawer(
+              onMenuClick: state.onMenuClick,
+            )
+          : const SizedBox(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => openWhatsApp(),
         elevation: 20.0,
@@ -42,24 +60,25 @@ class _HomeScreenState extends State<HomeScreen> {
           child: FaIcon(FontAwesomeIcons.whatsapp),
         ),
       ),
-      body: SingleChildScrollView(
-        controller: state.scrollController,
-        child: Column(
-          children: [
-            if (!isMobile) TopSection(onMenuClick: state.onMenuClick),
-            //  if (!isMobile) WebCarousel(pathToImages: bannerList),
-            // const SizedBox(height: kDefaultPaddingMd * 2),
-            AppointmentSection(key: state.keySection1),
-            TreatmentsSection(key: state.keySection2),
-            ProcedureSection(key: state.keySection3),
-            if (!isMobile) const FeedbackSection(),
-            AboutSection(key: state.keySection4),
-            QuestionsSection(questions: questions),
-            MapSection(key: state.keySection5),
-            FooterSection(onMenuClick: state.onMenuClick),
-          ],
+      body: Stack(children: [
+        SingleChildScrollView(
+          controller: state.scrollController,
+          child: Column(
+            children: [
+              //if (!isMobile) WebCarousel(pathToImages: bannerList),
+              const SizedBox(height: kDefaultPaddingMd * 2),
+              AppointmentSection(key: state.keySection1),
+              TreatmentsSection(key: state.keySection2),
+              ProcedureSection(key: state.keySection3),
+              if (!isMobile) const FeedbackSection(),
+              AboutSection(key: state.keySection4),
+              if (!isMobile) QuestionsSection(questions: questions),
+              MapSection(key: state.keySection5),
+              FooterSection(onMenuClick: state.onMenuClick),
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
