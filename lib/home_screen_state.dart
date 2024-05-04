dc5785855146f6c7131ad70c1e81f69354c3f864
AriCamargos <arianacamargos16@gmail.com>
2024-05-03 23:46:28 -0300

    refactor: menu section

--- lib/home_screen_state.dart
+++ lib/home_screen_state.dart
@@ -2,35 +2,13 @@ import 'package:flutter/material.dart';
 
 class MenuClickState extends ChangeNotifier {
   final scrollController = ScrollController();
-
-  final keySection1 = GlobalKey();
-  final keySection2 = GlobalKey();
-  final keySection3 = GlobalKey();
-  final keySection4 = GlobalKey();
-  final keySection5 = GlobalKey();
+  final List<GlobalKey<State<StatefulWidget>>> sectionKeys =
+      List.generate(6, (index) => GlobalKey());
 
   void onMenuClick(int value) {
-    final RenderBox renderBox;
+    final RenderBox renderBox =
+        sectionKeys[value].currentContext!.findRenderObject() as RenderBox;
 
-    switch (value) {
-      case 1:
-        renderBox = keySection1.currentContext!.findRenderObject() as RenderBox;
-        break;
-      case 2:
-        renderBox = keySection2.currentContext!.findRenderObject() as RenderBox;
-        break;
-      case 3:
-        renderBox = keySection3.currentContext!.findRenderObject() as RenderBox;
-        break;
-      case 4:
-        renderBox = keySection4.currentContext!.findRenderObject() as RenderBox;
-        break;
-      case 5:
-        renderBox = keySection5.currentContext!.findRenderObject() as RenderBox;
-        break;
-      default:
-        throw Exception();
-    }
     final offset = renderBox.localToGlobal(Offset.zero);
     scrollController.animateTo(offset.dy,
         duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
